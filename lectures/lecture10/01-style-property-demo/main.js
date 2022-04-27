d583145ac78a3ed9a586a3307d6b19394297e2ea
Andrew Rowan <andrewrowan2023@u.northwestern.edu>
2022-04-27 16:50:23 -0500

    Lecture 10

--- lectures/lecture10/01-style-property-demo/main.js
+++ lectures/lecture10/01-style-property-demo/main.js
@@ -1,21 +1,41 @@
+const resetButtonBackgrouds = () => {
+    document.querySelector("#btn1").style.backgroundColor = "rgb(133, 133, 133)";
+    document.querySelector("#btn2").style.backgroundColor = "rgb(133, 133, 133)";
+    document.querySelector("#btn3").style.backgroundColor = "rgb(133, 133, 133)";
+    document.querySelector("#btn4").style.backgroundColor = "rgb(133, 133, 133)";
+
+
+
+};
+
 
 const makeRed = () => {
-    // your code here...
+    resetButtonBackgrouds();
     console.log('Change background to red');
+    document.querySelector("body").style.backgroundColor = "red";
+    document.querySelector("#btn1").style.backgroundColor = "red";
 };
 
 const makeBlue = () => {
-    // your code here...
+    resetButtonBackgrouds();
     console.log('Change background to blue');
+    document.querySelector("body").style.backgroundColor = "blue";
+    document.querySelector("#btn2").style.backgroundColor = "blue";
+
+
 };
 
 const makePink = () => {
-    // your code here...
+    resetButtonBackgrouds();
     console.log('Change background to pink');
+    document.querySelector("body").style.backgroundColor = "hotpink";
+
 };
 
 const makeOrange = () => {
-    // your code here...
+    resetButtonBackgrouds();
     console.log('Change background to orange');
+    document.querySelector("body").style.backgroundColor = "orange";
+
 };
 
