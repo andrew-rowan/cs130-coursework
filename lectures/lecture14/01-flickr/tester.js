afc3e78da109364584de4288ce251ffe754d863b
Andrew Rowan <andrewrowan2023@u.northwestern.edu>
2022-05-11 16:37:51 -0500

    Lecture 14

---
+++ lectures/lecture14/01-flickr/tester.js
@@ -0,0 +1,33 @@
+let results;
+let greeting = "hey there";
+const serverURL = 'https://www.apitutor.org/flickr/simple/';
+
+const getPhotos = () => {
+    // this function pulls down data from a server
+    // everytime it is invoked. Change the value of
+    // the tag to get different data:
+    fetch(serverURL + '?tags=evanston')
+        .then(response => {
+            return response.json()
+        })
+        .then(doSomethingWithTheData);
+};
+
+const doSomethingWithTheData = (data) => {
+    // this function executes once the data has been 
+    // pulled down from the server:
+    results = data;
+    console.log('The Flickr data is stored in a variable called "results":')
+    console.log(data);
+
+
+    for (const item of results) {
+        // apend to DOM
+        document.querySelector('#gallery').innerHTML +=
+        `<img src="${item.img_url}" alt="${item.title}">`
+
+    }
+};
+
+// invoke the function when the page initializes:
+getPhotos();
